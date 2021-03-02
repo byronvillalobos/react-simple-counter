a21004055c522707103e3cc31ecbbd8d78e3735f
byronvillalobos <byronvillalobos@hotmail.com>
2021-03-02 00:03:16 +0000

    comments

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-//include bootstrap npm library into the bundle
+//include bootstrap npm library into the bundle//
 import "bootstrap";
 
 //include your index.scss file into the bundle
